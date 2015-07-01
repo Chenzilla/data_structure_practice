1a4eee3299c7937fd308905b9b247b0ab1cd019a
Alex Chen <alexschen94@gmail.com>
2015-07-01 00:07:35 -0500

    Add nodes to array structure

--- binary_tree.rb
+++ binary_tree.rb
@@ -3,17 +3,20 @@ class Node
 
   def initialize(value)
     @value = value
+    @left_child = false
+    @right_child = false
+    @parent = false
   end
 
-  def parent(node)
+  def set_parent(node)
     @parent = node
   end
 
-  def left_child(node)
+  def set_left_child(node)
     @left_child = node
   end
 
-  def right_child(node)
+  def set_right_child(node)
     @right_child = node
   end
 end
@@ -24,34 +27,38 @@ class Tree
 
   def initialize(array)
     @array = array
+    @tree = []
   end
 
   def build_tree
-    tree = []
     @array.shuffle!
     @array.each do |value|
-      tree = add_leaf(value, tree)
+      @tree = add_leaf(value, @tree, @tree[0])
     end
-    print tree
   end
 
-  def add_leaf(value, tree)
-    print tree
-    print tree.empty?
-    puts ''
+  def add_leaf(value, tree, parent)
     parent = tree[0]
-    if tree.empty?
-      print 'empty'
-      tree = [value, [], []]
-    elsif value < parent
-      tree[1] = add_leaf(value, tree[1])
-    elsif value > parent
-      tree[2] = add_leaf(value, tree[2])
+    if parent.nil?
+      tree = [Node.new(value), [], []]
+    elsif tree.nil?
+      node = Node.new(value)
+      node.set_parent(parent)
+      if node.value < parent.value
+        parent.set_left_child(node)
+      else
+        parent.set_right_child(node)
+      end
+    elsif value < parent.value
+      tree[1] = add_leaf(value, tree[1], parent)
+    elsif value > parent.value
+      tree[2] = add_leaf(value, tree[2], parent)
     end
     tree
   end
 
 end
+
 
 tree = Tree.new([1, 3, 2, 5, 4, 9])
 tree.build_tree
